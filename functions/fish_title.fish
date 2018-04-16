81fd4fc265562f0edff8b260e6132c976bcbc921
Max Milton <max@wearegenki.com>
2018-04-16 21:41:48 +1000

    Minor change to title

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,7 +1,7 @@
 function fish_title
   set pure_pwd (string replace $HOME "~" $PWD)
   if test -n "$argv"
-    echo -s (basename $pure_pwd) " : " $argv[1]
+    echo (basename $pure_pwd) ":" $argv[1]
   else
     echo $pure_pwd
   end
