ce636de1e50c85b76b42d788f65eb998ea4301c3
Max Milton <max@wearegenki.com>
2018-04-16 17:48:09 +1000

    Optimise time formting

--- functions/__pure_cmd_duration.fish
+++ functions/__pure_cmd_duration.fish
@@ -1,22 +1,18 @@
 function __pure_cmd_duration
-  if set -q CMD_DURATION
-    set full_seconds (math "$CMD_DURATION / 1000")
+  if test "$CMD_DURATION" -ge 5000
+    set milliseconds (math "$CMD_DURATION % 1000")
+    set seconds (math "$CMD_DURATION % 60")
+    set minutes (math "$CMD_DURATION / 60000 % 60")
+    set hours (math "$CMD_DURATION / 3600000 % 24")
 
-    if test $full_seconds -ge 5
-      set second_parts (math "$CMD_DURATION % 1000 / 10")
-      set seconds (math "$full_seconds % 60")
-      set minutes (math "$full_seconds / 60 % 60")
-      set hours (math "$full_seconds / 3600 % 24")
-
-      if test $hours -gt 0
-        echo -sn $hours "h "
-      end
-
-      if test $minutes -gt 0
-        echo -sn $minutes "m "
-      end
-
-      echo -s $seconds.$second_parts "s"
+    if test $hours -gt 0
+      echo -sn $hours "h "
     end
+
+    if test $minutes -gt 0
+      echo -sn $minutes "m "
+    end
+
+    echo -s $seconds.$milliseconds "s"
   end
 end
