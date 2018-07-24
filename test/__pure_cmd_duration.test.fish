e8e042e996ec8be02de942a31da7b13bbb55222e
Max Milton <max@wearegenki.com>
2018-07-25 07:22:32 +1000

    Add initial test setup

---
+++ test/__pure_cmd_duration.test.fish
@@ -0,0 +1,8 @@
+source $DIRNAME/../functions/__pure_cmd_duration.fish
+
+# test "set my_var default value"
+#   'default_value' = (
+#     __pure_set_default my_var 'default_value'
+#     echo $my_var
+#   )
+# end
