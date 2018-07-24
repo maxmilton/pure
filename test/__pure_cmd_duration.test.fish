b53e6ff4b0d97a4d199f414c460907e9746f8ba3
Max Milton <max@wearegenki.com>
2018-07-25 07:23:36 +1000

    Add ref comment

--- test/__pure_cmd_duration.test.fish
+++ test/__pure_cmd_duration.test.fish
@@ -1,5 +1,7 @@
 source $DIRNAME/../functions/__pure_cmd_duration.fish
 
+# FIXME: Write tests -- ref: https://github.com/fisherman/fishtape
+
 # test "set my_var default value"
 #   'default_value' = (
 #     __pure_set_default my_var 'default_value'
