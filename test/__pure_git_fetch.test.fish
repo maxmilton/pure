94d4e057f54d4fc5d5f3885ad85f5c5d05d2ca7d
Max Milton <max@wearegenki.com>
2018-07-25 21:05:21 +1000

    Add test placeholder

---
+++ test/__pure_git_fetch.test.fish
@@ -0,0 +1,11 @@
+source $DIRNAME/../functions/__pure_git_fetch.fish
+
+# FIXME: Add tests -- https://github.com/fisherman/fishtape
+
+test "placeholder"
+  # '' = (
+  #   __pure_git_fetch
+  # )
+
+  42 -eq (math 30 + 12)
+end
