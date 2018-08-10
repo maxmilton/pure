6d4c7ecc6c8c08bf8335807685c5c3f522af17dc
Max Milton <max@wearegenki.com>
2018-08-11 07:51:42 +1000

    Simplify duration fix

--- functions/__pure_cmd_duration.fish
+++ functions/__pure_cmd_duration.fish
@@ -1,7 +1,7 @@
 function __pure_cmd_duration
   set -l duration "$CMD_DURATION$cmd_duration"
 
-  if test -n "$duration" -a "$duration" -ge 5000
+  if test "$duration" -ge 5000
     set -l ms (math "$duration % 1000 / 10")
     set -l s (math "$duration / 1000 % 60")
     set -l m (math "$duration / 60000 % 60")
