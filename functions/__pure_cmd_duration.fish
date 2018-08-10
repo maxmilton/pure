78dc59c0797fc78a424da2a35e261b278fe012d2
Max Milton <max@wearegenki.com>
2018-08-11 07:14:02 +1000

    Potencial fix for issue #1

--- functions/__pure_cmd_duration.fish
+++ functions/__pure_cmd_duration.fish
@@ -1,9 +1,11 @@
 function __pure_cmd_duration
-  if test "$CMD_DURATION" -ge 5000
-    set -l ms (math "$CMD_DURATION % 1000 / 10")
-    set -l s (math "$CMD_DURATION / 1000 % 60")
-    set -l m (math "$CMD_DURATION / 60000 % 60")
-    set -l h (math "$CMD_DURATION / 3600000 % 24")
+  set -l duration "$CMD_DURATION$cmd_duration"
+
+  if test -n "$duration" -a "$duration" -ge 5000
+    set -l ms (math "$duration % 1000 / 10")
+    set -l s (math "$duration / 1000 % 60")
+    set -l m (math "$duration / 60000 % 60")
+    set -l h (math "$duration / 3600000 % 24")
 
     if test $h -gt 0
       echo -sn $h "h "
