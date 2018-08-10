3919a369c66d6030f2a794ba50b26710d311b0b7
Max Milton <max@wearegenki.com>
2018-08-11 07:52:29 +1000

    Fix issue with process vars removed in fish 3 (no %self)

--- functions/__pure_update_prompt.fish
+++ functions/__pure_update_prompt.fish
@@ -1,4 +1,5 @@
 function __pure_update_prompt
-  set -l cmd "kill -WINCH "(echo %self)
+  set -q pid; or set pid %self # fish shell 3.x compat
+  set -l cmd "kill -WINCH $pid"
   fish -c "$cmd" &
 end
